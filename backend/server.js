5a4e6a9867cd5e851ff646cb66b95b460a176c89
Brijesh <brijeshp3349@gmail.com>
2025-03-08 15:08:49 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import { authRoutes } from "./routes/authRoutes.js";
 import { recipesRouter } from "./routes/recipes.js";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
@@ -22,5 +22,6 @@ connectDB();
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+    // console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running ...`);
 });
